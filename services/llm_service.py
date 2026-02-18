58f885496849db5e07f5b826bd91c6f58993052e
muhammedhassain <hassainmuhammed66@gmail.com>
2026-02-18 11:57:05 +0530

    feat: production ready secure architecture

---
+++ services/llm_service.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+
+load_dotenv()
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+if not GROQ_API_KEY:
+    raise ValueError("GROQ_API_KEY missing in environment")
+
+llm = ChatGroq(
+    groq_api_key=GROQ_API_KEY,
+    model_name="llama-3.1-8b-instant",
+    temperature=0.2
+)
